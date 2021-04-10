3360989f8d388e8e2fd4c8453c61621291e167dd
rolandmontalvan <rolandfilho@gmail.com>
2021-04-10 11:59:34 -0300

    src com documentos

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 import PyPDF2
 
 pdfFileObj = open('/home/rlnd/Downloads/Evaluating Energy Efficiency and Security for Internet of Things A Systematic Review.pdf', 'rb')
@@ -11,3 +12,29 @@ pageObj = pdfReader.getPage(0)
 print(pageObj.extractText())
 
 pdfFileObj.close()
+'''
+from PyPDF2 import PdfFileReader
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    txt = f"""
+    Information about {pdf_path}: 
+
+    Author: {information.author}
+    Creator: {information.creator}
+    Producer: {information.producer}
+    Subject: {information.subject}
+    Title: {information.title}
+    Number of pages: {number_of_pages}
+    """
+
+    print(txt)
+    return information
+
+if __name__ == '__main__':
+    path = './src/documents/Evaluating Energy Efficiency and Security for Internet of Things A Systematic Review.pdf'
+    extract_information(path)
